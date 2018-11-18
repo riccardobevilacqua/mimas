10a6fdc7f01c7ad2a1a9cf66bc410fc078b61199
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2018-11-18 01:17:00 +0100

    Added channels handling

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,26 @@ const client = new Discord.Client();
 
 client.once('ready', () => console.log('Ready!'));
 
+client.on('message', message => {
+    if (message.content === '!channels') {
+        client.channels
+            .filter(channel => channel.type === 'voice')
+            .forEach(channel => {
+                message.channel.send(channel.id + ' | ' + channel.type);
+            });
+    }
+});
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.voiceChannel
+    let oldUserChannel = oldMember.voiceChannel
+
+
+    if(oldUserChannel === undefined && newUserChannel !== undefined) {
+        console.log('A user has just joined channel ' + newMember.voiceChannelID);
+    } else if(newUserChannel === undefined){
+        console.log('A user has just left channel ' + newMember.voiceChannelID);
+    }
+});
+
 client.login(token);
