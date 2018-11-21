eed026d2d9d8c417946a68fc609c19a6b3c27f84
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2018-11-21 22:06:57 +0100

    Added channels sorting

--- src/index.ts
+++ src/index.ts
@@ -13,23 +13,37 @@ const getEmptyVoiceChannelClones = (voiceChannel: Discord.VoiceChannel): Discord
     return getVoiceChannelClones(voiceChannel).filter((channel: Discord.VoiceChannel) => channel.members.size < 1);
 };
 
-client.once('ready', () => console.log('Ready!'));
+const cloneVoiceChannel = (voiceChannel: Discord.VoiceChannel) => {
+    if (!!voiceChannel && voiceChannel.members.size > 0 && getEmptyVoiceChannelClones(voiceChannel).size < 1) {
+        voiceChannel.clone()
+            .then((clonedChannel: Discord.VoiceChannel) => {
+                clonedChannel.setParent(voiceChannel.parentID);
+                clonedChannel.edit({
+                    userLimit: voiceChannel.userLimit,
+                    position: voiceChannel.position + 1
+                });
+            })
+            .catch((error: Error) => console.log(error));
+    }
+};
+
+client.once('ready', () => {
+    console.log('Ready!');
+    client.guilds.forEach((guild: Discord.Guild) => {
+        if (guild.available) {
+            guild.channels.filter((channel: Discord.Channel) => channel.type === 'voice')
+                .forEach((voiceChannel: Discord.GuildChannel) => {
+                    voiceChannel.edit({position: (voiceChannel.position + 1) * 100});
+                });
+        }
+    });
+});
 
 client.on('voiceStateUpdate', (oldMember: Discord.GuildMember, newMember: Discord.GuildMember): void => {
     const newVoiceChannel: Discord.VoiceChannel = newMember.voiceChannel;
     const oldVoiceChannel: Discord.VoiceChannel = oldMember.voiceChannel;
     
-    if (!!newVoiceChannel && newVoiceChannel.members.size > 0 && getEmptyVoiceChannelClones(newVoiceChannel).size < 1) {
-        newVoiceChannel.clone()
-            .then((clonedChannel: Discord.VoiceChannel) => {
-                clonedChannel.setParent(newVoiceChannel.parentID);
-                clonedChannel.edit({
-                    userLimit: newVoiceChannel.userLimit,
-                    position: newVoiceChannel.position
-                });
-            })
-            .catch((error: Error) => console.log(error));
-    }
+    cloneVoiceChannel(newVoiceChannel);
 
     if (!!oldVoiceChannel && oldVoiceChannel.members.size < 1 && getEmptyVoiceChannelClones(oldVoiceChannel).size > 0) {
         oldVoiceChannel.delete();
