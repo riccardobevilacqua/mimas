b4e773a0211e8fe8de9f6b17bd34347490aeb425
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2018-11-25 21:26:41 +0100

    Fixed clones handling

--- src/index.ts
+++ src/index.ts
@@ -7,50 +7,68 @@ import { getVoiceChannelClones, getEmptyVoiceChannelClones, hasCloningPermission
 const client: Eris.Client = new Eris.Client(token);
 const guildRegistry: GuildRegistry = new GuildRegistry();
 
-const cloneVoiceChannel = (voiceChannel: Eris.VoiceChannel) => {
+const cloneVoiceChannel = (voiceChannel: Eris.VoiceChannel): void => {
     try {
-        guildRegistry.addGuild(voiceChannel.guild.id);
-        const registeredGuild = guildRegistry.findGuild(voiceChannel.guild.id);
-                
-        if (!!voiceChannel && voiceChannel.voiceMembers.size > 0 
-            && getEmptyVoiceChannelClones(voiceChannel).length < 1 
-            && hasCloningPermissions(voiceChannel, client.user) 
-            && !registeredGuild.cloningLock) {
-            guildRegistry.toggleCloningLock(registeredGuild.id);
-
-            client.createChannel(registeredGuild.id, voiceChannel.name, 2, null, voiceChannel.parentID)
-                .then((channel: Eris.AnyGuildChannel) => {
-                    (<Eris.VoiceChannel>channel).edit({
-                        userLimit: voiceChannel.userLimit
-                    });
-                    // channel.editPosition(voiceChannel.position);
-                })
-                .then(() => {
-                    guildRegistry.toggleCloningLock(registeredGuild.id);
-                })
-                .catch((error) => console.log(error));
+        if (!!voiceChannel) {
+            guildRegistry.addGuild(voiceChannel.guild.id);
+            const registeredGuild = guildRegistry.findGuild(voiceChannel.guild.id);
+                    
+            if (voiceChannel.voiceMembers.size > 0 
+                && getEmptyVoiceChannelClones(voiceChannel).length < 1 
+                && hasCloningPermissions(voiceChannel, client.user) 
+                && !registeredGuild.cloningLock) {
+                guildRegistry.toggleCloningLock(registeredGuild.id);
+    
+                client.createChannel(registeredGuild.id, voiceChannel.name, 2, null, voiceChannel.parentID)
+                    .then((channel: Eris.AnyGuildChannel) => {
+                        (<Eris.VoiceChannel>channel).edit({
+                            userLimit: voiceChannel.userLimit
+                        });
+                        // channel.editPosition(voiceChannel.position);
+                    })
+                    .then(() => {
+                        guildRegistry.toggleCloningLock(registeredGuild.id);
+                    })
+                    .catch((error) => console.log(error));
+            }
         }
     } catch (error) {
         console.log(error);
     }
 };
 
-client.on('voiceStateUpdate', (member: Eris.Member, oldState: Eris.VoiceState): void => {
+const removeCloneVoiceChannel = (voiceChannel: Eris.VoiceChannel): void => {
     try {
-        if (!!member.voiceState.channelID) {
-            if (!!member.voiceState.channelID) {
-                const newVoiceChannel: Eris.VoiceChannel = <Eris.VoiceChannel>client.getChannel(member.voiceState.channelID);
-                cloneVoiceChannel(newVoiceChannel);
-            }
-
-            if (!!oldState.channelID) {
-                const oldVoiceChannel: Eris.VoiceChannel = (<Eris.VoiceChannel>client.getChannel(oldState.channelID));
-
-                if (!!oldVoiceChannel && oldVoiceChannel.voiceMembers.size < 1 && getEmptyVoiceChannelClones(oldVoiceChannel).length > 0) {
-                    oldVoiceChannel.delete();
-                }
+        if (!!voiceChannel) {
+            if (!!voiceChannel && voiceChannel.voiceMembers.size < 1 && getEmptyVoiceChannelClones(voiceChannel).length > 0) {
+                voiceChannel.delete();
             }
         }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+client.on('voiceChannelJoin', (member: Eris.Member, newVoiceChannel: Eris.VoiceChannel): void => {
+    try {
+        cloneVoiceChannel(newVoiceChannel);
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+client.on('voiceChannelLeave', (member: Eris.Member, oldVoiceChannel: Eris.VoiceChannel): void => {
+    try {
+        removeCloneVoiceChannel(oldVoiceChannel);
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+client.on('voiceChannelSwitch', (member: Eris.Member, newVoiceChannel: Eris.VoiceChannel, oldVoiceChannel: Eris.VoiceChannel): void => {
+    try {
+        cloneVoiceChannel(newVoiceChannel);
+        removeCloneVoiceChannel(oldVoiceChannel);
     } catch(error) {
         console.log(error);
     }
