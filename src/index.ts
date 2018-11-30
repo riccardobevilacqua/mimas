4fcda9f38f8bfe0fd1e4b284ed39cf0dc223a193
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2018-11-30 22:44:16 +0100

    Removed unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,6 @@ import { ClientChannelManager } from './clientChannelManager';
 
 const client: ClientChannelManager = new ClientChannelManager();
 
-// client.on('voiceChannelJoin', (member: Eris.Member, newVoiceChannel: Eris.VoiceChannel): void => {
-//     try {
-//         client.cloneVoiceChannel(newVoiceChannel);
-//     } catch(error) {
-//         console.log(error);
-//     }
-// });
-
-// client.on('voiceChannelLeave', (member: Eris.Member, oldVoiceChannel: Eris.VoiceChannel): void => {
-//     try {
-//         client.removeCloneVoiceChannel(oldVoiceChannel);
-//     } catch(error) {
-//         console.log(error);
-//     }
-// });
-
-// client.on('voiceChannelSwitch', (member: Eris.Member, newVoiceChannel: Eris.VoiceChannel, oldVoiceChannel: Eris.VoiceChannel): void => {
-//     try {
-//         client.cloneVoiceChannel(newVoiceChannel);
-//         client.removeCloneVoiceChannel(oldVoiceChannel);
-//     } catch(error) {
-//         console.log(error);
-//     }
-// });
-
 client.on('voiceStateUpdate', (oldMember: Discord.GuildMember, newMember: Discord.GuildMember) => {
     try {    
         if (newMember.voiceChannel) {
