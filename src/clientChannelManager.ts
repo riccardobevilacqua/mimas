fb641f202e4666e5822d96e3df7571d5410eccaa
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2019-10-24 20:32:33 +0200

    Removed console log

--- src/clientChannelManager.ts
+++ src/clientChannelManager.ts
@@ -62,18 +62,12 @@ export class ClientChannelManager extends Discord.Client {
         const categoryVoiceChannels: Discord.VoiceChannel[] = [...this.getCategoryVoiceChannels(voiceChannel)];
         const categoryEmptyVoiceChannels: Discord.VoiceChannel[] = categoryVoiceChannels.filter((item: Discord.VoiceChannel) => item.members.size === 0);
 
-        console.info(`CATEGORY VOICE CHANNELS: ${categoryVoiceChannels}`);
-        console.info(`CATEGORY EMPTY VOICE CHANNELS: ${categoryEmptyVoiceChannels}`);
         if (prevChannelList.length > 0) {
-            console.log('PREV CHANNELS DETECTED');
             const refChannelName: string = prevChannelList.pop();
-            console.log(`REF CHANNEL: ${refChannelName}`);
             const refChannels: Discord.VoiceChannel[] = categoryVoiceChannels
                 .filter((item: Discord.VoiceChannel) => 
                     item.name === refChannelName && item.id !== voiceChannel.id && item.members.size > 0
                 );
-
-            console.info(`REF CHANNELS: ${refChannels}`);
             
             if (refChannels.length > 0) {
                 voiceChannel.setPosition(refChannels.pop().position);
@@ -82,7 +76,6 @@ export class ClientChannelManager extends Discord.Client {
             }
         } else {
             const lastChannel = categoryEmptyVoiceChannels.pop();
-            console.log(`LAST CHANNEL [${lastChannel}] | POS [${lastChannel.position}]`);
             voiceChannel.setPosition(lastChannel.position);
         }
     }
@@ -90,8 +83,7 @@ export class ClientChannelManager extends Discord.Client {
     moveJoinedChannel(voiceChannel: Discord.VoiceChannel): void {
         const uniqueChannels: string[] = this.getUniqueChannels(voiceChannel);
         const prevChannelList: string[] = uniqueChannels.slice(0, uniqueChannels.indexOf(voiceChannel.name)).reverse();
-        console.info(`UNIQUE CHANNELS: ${uniqueChannels}`);
-        console.info(`PREV CHANNELS: ${prevChannelList}`);
+        
         this.injectVoiceChannel(voiceChannel, prevChannelList);
     }
 
@@ -131,7 +123,6 @@ export class ClientChannelManager extends Discord.Client {
                                             value
                                                 .setPosition(voiceChannel.position)
                                                 .then(() => {
-                                                    console.log('===================');
                                                     if (!self.isLastVoiceChannel(voiceChannel)) {
                                                         self.moveJoinedChannel(voiceChannel);
                                                     }
