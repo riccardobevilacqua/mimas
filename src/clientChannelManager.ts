07ae7a6952ec7730699038bca5c38360af80a155
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2019-10-27 20:01:13 +0100

    Updated voice channel injection

--- src/clientChannelManager.ts
+++ src/clientChannelManager.ts
@@ -6,99 +6,100 @@ export class ClientChannelManager extends Discord.Client {
 
     getVoiceChannelClones(originalChannel: Discord.VoiceChannel): Discord.VoiceChannel[] {
         return <Discord.VoiceChannel[]>originalChannel.guild.channels
-            .filter((channel: Discord.GuildChannel) => {
-                return channel.type === 'voice' 
-                    && channel.name === originalChannel.name 
-                    && channel.id !== originalChannel.id 
-                    && channel.parentID === originalChannel.parentID
+            .filter(item => {
+                return item.type === 'voice' 
+                    && item.name === originalChannel.name 
+                    && item.id !== originalChannel.id 
+                    && item.parentID === originalChannel.parentID
             })
             .array();
     }
     
-    getEmptyVoiceChannelClones(originalChannel: Discord.VoiceChannel): Discord.VoiceChannel[] {
-        return this.getVoiceChannelClones(originalChannel)
-            .filter((channel: Discord.VoiceChannel) => channel.members.size === 0);
+    getEmptyVoiceChannelClones(voiceChannel: Discord.VoiceChannel): Discord.VoiceChannel[] {
+        return this.getVoiceChannelClones(voiceChannel).filter(item => item.members.size === 0);
     }
     
     hasPermissions(voiceChannel: Discord.VoiceChannel): boolean {
         const categoryPermissions: Readonly<Discord.Permissions> = voiceChannel.parent.permissionsFor(this.user);
         const channelPermissions: Readonly<Discord.Permissions> = voiceChannel.permissionsFor(this.user);
-        let result = false;
         
         if (categoryPermissions.has('MANAGE_CHANNELS') && channelPermissions.has('VIEW_CHANNEL')) {
-            result = true;
+            return true;
         }
     
-        return result;
+        return false;
     }
 
     comparePosition(a: Discord.GuildChannel, b: Discord.GuildChannel): number {
-        let result: number = 0;
-
         if (a.position < b.position) {
-            result = -1;
+            return -1;
         } else if (a.position > b.position) {
-            result = 1;
+            return 1;
         }
 
-        return result;
+        return 0;
     }
 
     getCategoryVoiceChannels(channel: Discord.VoiceChannel): Discord.VoiceChannel[] {
         return <Discord.VoiceChannel[]>channel.parent.children
             .array()
-            .filter((guildChannel: Discord.GuildChannel) => guildChannel.type === 'voice')
+            .filter(guildChannel => guildChannel.type === 'voice')
             .sort(this.comparePosition);
     }
 
     getUniqueChannels(channel: Discord.VoiceChannel): string[] {
         const categoryChannelNames: string[] = this
             .getCategoryVoiceChannels(channel)
-            .map((item: Discord.VoiceChannel) => item.name);
+            .map(item => item.name);
         return [...new Set(categoryChannelNames)];
     }
 
-    injectVoiceChannel(voiceChannel: Discord.VoiceChannel, prevChannelList: string[]) {
+    async injectVoiceChannel(voiceChannel: Discord.VoiceChannel, prevChannelList: string[]) {
         const categoryVoiceChannels: Discord.VoiceChannel[] = [...this.getCategoryVoiceChannels(voiceChannel)];
-        const categoryEmptyVoiceChannels: Discord.VoiceChannel[] = categoryVoiceChannels.filter((item: Discord.VoiceChannel) => item.members.size === 0);
+        const categoryEmptyVoiceChannels: Discord.VoiceChannel[] = categoryVoiceChannels.filter(item => item.members.size === 0);
 
         if (prevChannelList.length > 0) {
             const refChannelName: string = prevChannelList.pop();
             const refChannels: Discord.VoiceChannel[] = categoryVoiceChannels
-                .filter((item: Discord.VoiceChannel) => 
+                .filter(item => 
                     item.name === refChannelName && item.id !== voiceChannel.id && item.members.size > 0
                 );
             
             if (refChannels.length > 0) {
-                voiceChannel.setPosition(refChannels.pop().position);
+                await voiceChannel.setPosition(refChannels.pop().position);
             } else {
                 this.injectVoiceChannel(voiceChannel, prevChannelList);
             }
         } else {
             const lastChannel = categoryEmptyVoiceChannels.pop();
-            voiceChannel.setPosition(lastChannel.position);
+            await voiceChannel.setPosition(lastChannel.position);
         }
     }
 
     moveJoinedChannel(voiceChannel: Discord.VoiceChannel): void {
         const uniqueChannels: string[] = this.getUniqueChannels(voiceChannel);
         const prevChannelList: string[] = uniqueChannels.slice(0, uniqueChannels.indexOf(voiceChannel.name)).reverse();
+
+        uniqueChannels.forEach(item => console.log(`DETECTED UNIQUE CHANNEL [${item}]`));
+        prevChannelList.forEach(item => console.log(`DETECTED PREVIOUS CHANNEL [${item}]`));
         
         this.injectVoiceChannel(voiceChannel, prevChannelList);
     }
 
     isLastVoiceChannel(voiceChannel: Discord.VoiceChannel): boolean {
         const channelsList: Discord.VoiceChannel[] = [...this.getCategoryVoiceChannels(voiceChannel)];
-        let result = false;
 
         if (channelsList && channelsList.length > 0 && voiceChannel.position === channelsList.pop().position) {
-            result = true;
+            console.log(`CHANNEL [${voiceChannel.name}] is the last`);
+            return true;
         }
 
-        return result;
+        console.log(`CHANNEL [${voiceChannel.name}] is NOT the last`);
+
+        return false;
     }
 
-    cloneVoiceChannel(voiceChannel: Discord.VoiceChannel): void {
+    async cloneVoiceChannel(voiceChannel: Discord.VoiceChannel) {
         try {
             const self: ClientChannelManager = this;
             if (voiceChannel && self.hasPermissions(voiceChannel)) {
@@ -111,31 +112,17 @@ export class ClientChannelManager extends Discord.Client {
                     && !registeredGuild.cloningLock) {
                     self.guildRegistry.toggleCloningLock(registeredGuild.id);
 
-                    voiceChannel
-                        .clone()
-                        .then((createdChannel: Discord.VoiceChannel) => {
-                            createdChannel
-                                .setUserLimit(voiceChannel.userLimit)
-                                .then(value => {
-                                    value
-                                        .setParent(voiceChannel.parentID)
-                                        .then(value => {
-                                            value
-                                                .setPosition(voiceChannel.position)
-                                                .then(() => {
-                                                    if (!self.isLastVoiceChannel(voiceChannel)) {
-                                                        self.moveJoinedChannel(voiceChannel);
-                                                    }
-                                                })
-                                        });
-                                })
-                                .catch((error) => console.log(error));
-                        })
-                        .then(() => self.guildRegistry.toggleCloningLock(registeredGuild.id))
-                        .catch((error) => {
-                            self.guildRegistry.toggleCloningLock(registeredGuild.id);
-                            console.log(error);
-                        });
+                    const createdChannel: Discord.VoiceChannel = <Discord.VoiceChannel>(await voiceChannel.clone());
+
+                    await createdChannel.setParent(voiceChannel.parentID);
+                    await createdChannel.setUserLimit(voiceChannel.userLimit);
+                    await createdChannel.setPosition(voiceChannel.position);
+
+                    if (!self.isLastVoiceChannel(voiceChannel)) {
+                        self.moveJoinedChannel(voiceChannel);
+                    }
+
+                    await self.guildRegistry.toggleCloningLock(registeredGuild.id);
                 }
             }
         } catch (error) {
