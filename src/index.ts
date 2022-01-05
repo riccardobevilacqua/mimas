087b3a5983cc6708687d89c1032f890dcce4d04b
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2022-01-05 01:21:16 +0100

    Updated types in voiceStateUpdate even handler

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,17 @@ import { ClientChannelManager } from './clientChannelManager';
 Dotenv.config();
 Http.createServer().listen(3000);
 
-const client: ClientChannelManager = new ClientChannelManager();
+const client: ClientChannelManager = new ClientChannelManager({ intents: [Discord.Intents.FLAGS.GUILDS] });
 
-client.on('voiceStateUpdate', (oldState: Discord.GuildMember, newState: Discord.GuildMember) => {
+client.on('voiceStateUpdate', (oldState: Discord.VoiceState, newState: Discord.VoiceState) => {
     try {
-        if (newState.voiceChannelID !== oldState.voiceChannelID) {
-            if (newState.voiceChannel) {
-                client.cloneVoiceChannel(newState.voiceChannel);
+        if (newState?.channelId !== oldState?.channelId) {
+            if (newState.channel) {
+                client.cloneVoiceChannel(<Discord.VoiceChannel>newState.channel);
             }
-            
-            if (oldState.voiceChannel) {
-                client.removeCloneVoiceChannel(oldState.voiceChannel);
+
+            if (oldState.channel) {
+                client.removeCloneVoiceChannel(<Discord.VoiceChannel>oldState.channel);
             }
         }
     } catch(error) {
@@ -44,6 +44,6 @@ client.on('guildDelete', (guild: Discord.Guild) => {
     }
 });
 
-client.on('ready', () => console.log('Ready!'));
+client.once('ready', () => console.log('Ready!'));
 
 client.login(process.env.TOKEN);
